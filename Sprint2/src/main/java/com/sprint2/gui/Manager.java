f11e3afa1174a89cd31d647a5d05ae2a3cc0f5d0
ninuljaja <93230927+ninuljaja@users.noreply.github.com>
2023-04-13 12:16:00 -0400

    q

--- Sprint2/src/main/java/com/sprint2/gui/Manager.java
+++
@@ -1,8 +0,0 @@
-package com.sprint2.gui;
-
-public class Manager extends Employee {
-    private String firstName;
-    private String lastName;
-    private String login;
-    private String password;
-}
---
+++ src/main/java/com/sprint2/gui/Manager.java
@@ -0,0 +1,8 @@
+package com.sprint2.gui;
+
+public class Manager extends Employee {
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+}
