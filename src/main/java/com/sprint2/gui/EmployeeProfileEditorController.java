6c4e74d5af9bacb6090c194d7ca1b21a006d94f4
Relaxgineer <107740082+piemonk99@users.noreply.github.com>
2023-04-26 18:15:39 -0400

    Started configuring table for employee profile editor

--- src/main/java/com/sprint2/gui/EmployeeProfileEditorController.java
+++ src/main/java/com/sprint2/gui/EmployeeProfileEditorController.java
@@ -1,5 +1,44 @@
 package com.sprint2.gui;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+
 public class EmployeeProfileEditorController {
 
+    @FXML
+    private TableView employeeTable;
+    @FXML
+    private TableColumn employee, role;
+
+
+
+
+    public void initialize() {
+        Session session = Session.getInstance();
+        Employee user = session.getUser();
+
+        ObservableList<Employee> data = FXCollections.observableArrayList();
+
+    }
+
+    public void onGoBackBtn(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginAs.fxml"));
+        Parent root = loader.load();
+        // Get the current node and its parent scene
+        Node node = (Node) actionEvent.getSource();
+        Scene scene = node.getScene();
+        scene.setRoot(root);
+    }
 }
